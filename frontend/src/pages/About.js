dccc0c220c561a79ecc31da7e3a06206bd9909f9
Liat <liathirsh@gmail.com>
2023-02-08 10:22:55 -0500

    Added an About page

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,42 @@
+export function About() {
+  return (
+    <div>
+      <h1> About Us </h1>
+      <p>
+        {" "}
+        We are a community-driven website dedicated to showcasing the best and
+        most popular Python packages. Our mission is to make it easy for
+        developers to discover and use the tools they need to build great
+        software.
+      </p>
+      <br></br>
+      <p>
+        {" "}
+        We believe that the Python ecosystem is one of the richest and most
+        diverse of any programming language, and we are proud to help shine a
+        light on the many talented developers who contribute to it.{" "}
+      </p>
+      <br> </br>
+      <p>
+        {" "}
+        You can contribute to this project by voting on the Python packages you
+        work with and prefer. Making it easier for newer developers to know
+        where to begin when starting a project.{" "}
+      </p>
+      {/* <p>
+        {" "}
+        Our team of experts review and curate the packages on our site to ensure
+        that they meet the highest standards of quality and functionality. We
+        also provide detailed documentation and examples to help users get up
+        and running quickly.
+      </p> */}
+      <p>
+        Whether you're a seasoned Python developer or just starting out, we hope
+        that our site will be a valuable resource for you. So why wait? Explore
+        our collection of packages today and start building your next great
+        project!{" "}
+      </p>
+      <p> All text was generated on this website using ChatGPT</p>
+    </div>
+  );
+}
